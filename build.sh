1ac3e72c6c77a66754c1c5dbf334cebccc3a03f2
cccc <ChockChocsChoirChoke@protonmail.com>
2022-03-08 19:25:45 -0500

    Musl builds

--- build.sh
+++ build.sh
@@ -3,9 +3,15 @@
 set -ex
 
 export RUSTFLAGS='-C target-feature=+crt-static'
-cargo build --release --target x86_64-unknown-linux-gnu
+# cargo build --release --target x86_64-unknown-linux-gnu
+# cargo build --release
+cargo build --release --target x86_64-unknown-linux-musl
 
-mkdir dist/ || true
-mv target/x86_64-unknown-linux-gnu/release/main dist/dmap.fat
+rm -rf dist/
+mkdir dist/
+# mv target/x86_64-unknown-linux-gnu/release/main dist/dmap.fat
+# mv target/release/main dist/dmap.fat
+mv target/x86_64-unknown-linux-musl/release/main dist/dmap.fat
 
-strip -o dist/dmap dist/dmap.fat
+strip -o dist/dmap.stripped dist/dmap.fat
+upx --best -o dist/dmap dist/dmap.stripped
